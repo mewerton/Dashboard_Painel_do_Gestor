2ec8d01666d61858e8ac38be286dad16df501747
Mewerton Melo <mewerton@gmail.com>
2024-08-29 13:00:41 -0300

    Ajustando a tabela para fazer pesquisa, aplicar filtros e mostrar soma dos valores filtrados

--- despesas_ug.py
+++ despesas_ug.py
@@ -28,80 +28,7 @@ def run_dashboard():
         else:
             return locale.currency(value, grouping=True)
 
-    # Função para ajustar os dados dos anos da COVID-19 para a predição
-    def adjust_for_covid(df, covid_years, pre_covid_years, post_covid_years):
-        adjusted_df = df.copy()
-        for year in covid_years:
-            pre_covid_data = df[df['ANO'].isin(pre_covid_years)]
-            post_covid_data = df[df['ANO'].isin(post_covid_years)]
-            pre_covid_avg = pre_covid_data.groupby(['MES'])['VALOR_PAGO'].mean().reset_index()
-            post_covid_avg = post_covid_data.groupby(['MES'])['VALOR_PAGO'].mean().reset_index()
-            
-            avg_crescent = pd.DataFrame()
-            avg_crescent['MES'] = pre_covid_avg['MES']
-            avg_crescent['VALOR_PAGO'] = (pre_covid_avg['VALOR_PAGO'] + post_covid_avg['VALOR_PAGO']) / 2
-            
-            adjusted_df.loc[adjusted_df['ANO'] == year, 'VALOR_PAGO'] = adjusted_df[adjusted_df['ANO'] == year]['MES'].map(avg_crescent.set_index('MES')['VALOR_PAGO'])
-        
-        return adjusted_df
-
-    # Anos de COVID e períodos pré e pós COVID
-    covid_years = [2020, 2021]
-    pre_covid_years = [2018, 2019]
-    post_covid_years = [2022, 2023]
-
-    # Ajustar os dados para a predição
-    df_for_prediction = adjust_for_covid(df, covid_years, pre_covid_years, post_covid_years)
-
-    # Função para realizar análise preditiva por mês e calcular a acurácia
-    def perform_predictive_analysis_by_month(df):
-        future_data = []
-        mae_values = []
-        mape_values = []
-
-        for mes in range(1, 13):
-            df_mes = df[df['MES'] == mes]
-
-            if df_mes.empty:
-                continue
-
-            # Preparar os dados para a regressão linear
-            X = df_mes['ANO'].values.reshape(-1, 1)
-            y = df_mes['VALOR_PAGO'].values
-
-            # Verificar se existem valores zero em y para evitar divisão por zero
-            if np.any(y == 0):
-                continue
-
-            # Treinar o modelo de regressão linear
-            model = LinearRegression()
-            model.fit(X, y)
-
-            # Fazer previsões para o próximo ano
-            next_year = df_mes['ANO'].max() + 1
-            future_prediction = model.predict(np.array([[next_year]]))[0]
-
-            # Calcular a acurácia (MAE) usando cross-validation
-            y_pred = model.predict(X)
-            mae = mean_absolute_error(y, y_pred)
-            mae_values.append(mae)
-
-            # Calcular o MAPE
-            mape = np.mean(np.abs((y - y_pred) / y)) * 100
-            mape_values.append(mape)
-
-            # Armazenar o resultado
-            future_data.append({'MES': mes, 'ANO': next_year, 'VALOR_PAGO_PREVISTO': future_prediction})
-
-        # Calcular a média do MAE e do MAPE para todos os meses
-        avg_mae = np.mean(mae_values) if mae_values else float('nan')
-        avg_mape = np.mean(mape_values) if mape_values else float('nan')
-
-        # Criar um DataFrame com as previsões futuras
-        future_df = pd.DataFrame(future_data)
-
-        return future_df, avg_mae, avg_mape
-
+   
     # Carregar o sidebar
     selected_ugs_despesas, selected_ano, selected_mes = load_sidebar(df, "despesas_ug")
 
@@ -109,7 +36,6 @@ def run_dashboard():
         # Filtrar dados apenas para o Poder Executivo
         df = df[df['PODER'] == 'EXE']
 
-    
 
     # Aplicar filtros ao dataframe
     df_filtered = df[df['UG'].isin(selected_ugs_despesas)]
@@ -311,6 +237,37 @@ def run_dashboard():
     st.subheader('Despesas - Detalhado')
     df_detalhado = df_filtered[['DESCRICAO_NATUREZA', 'NOME_FAVORECIDO', 'TIPO_LICITACAO', 'UG_EMITENTE', 'NOTA_EMPENHO', 'COD_PROCESSO', 'NOME_CONTRATO', 'OBSERVACAO_NE', 'VALOR_PAGO']]
 
+    # Campo de entrada para a palavra-chave de pesquisa
+    keyword = st.text_input('Digite uma palavra-chave para filtrar a tabela:')
+
+    # Filtrar o dataframe com base na palavra-chave
+    if keyword:
+        df_detalhado = df_detalhado[df_detalhado.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+
+    # Opções de exibição de valores
+    col7, col8, col9 = st.columns(3)
+
+    with col7:
+        exibir_positivos = st.checkbox('Exibir valores positivos', value=True)
+    
+    with col8:
+        exibir_zerados = st.checkbox('Exibir valores zerados', value=True)
+    
+    with col9:
+        exibir_negativos = st.checkbox('Exibir valores negativos', value=True)
+    
+
+    # Filtrar o dataframe com base nas opções de exibição
+    if not exibir_positivos:
+        df_detalhado = df_detalhado[df_detalhado['VALOR_PAGO'] <= 0]
+    if not exibir_zerados:
+        df_detalhado = df_detalhado[df_detalhado['VALOR_PAGO'] != 0]
+    if not exibir_negativos:
+        df_detalhado = df_detalhado[df_detalhado['VALOR_PAGO'] >= 0]
+
+    # Calcular o valor total das linhas filtradas
+    valor_total_filtrado = df_detalhado['VALOR_PAGO'].sum()
+
     # Configurar a formatação de valores na exibição usando o st.dataframe
     st.dataframe(
         df_detalhado.rename(columns={
@@ -326,75 +283,9 @@ def run_dashboard():
         }).style.format({'Valor Pago': 'R$ {:,.2f}'})
     )
 
-    # Realizar a análise preditiva por mês e obter o MAE e o MAPE
-    df_meses_ano = df_for_prediction.groupby(['MES', 'ANO'])['VALOR_PAGO'].sum().reset_index()
-    future_df_by_month, avg_mae, avg_mape = perform_predictive_analysis_by_month(df_meses_ano)
+    # Exibir o valor total das linhas filtradas
+    st.markdown(f"**Valor total das linhas filtradas:** R$ {valor_total_filtrado:,.2f}")
 
-    # Combinar dados históricos e previsões
-    combined_df = pd.concat([df_meses_ano, future_df_by_month.rename(columns={'VALOR_PAGO_PREVISTO': 'VALOR_PAGO'})], ignore_index=True)
-
-    # Gráfico de previsões futuras combinadas
-    fig_combined = px.line(combined_df, x='MES', y='VALOR_PAGO', color='ANO', title='Comparação de Despesas nos Últimos Anos e Previsão de Despesas para os Próximos 12 Meses usando Inteligência Artificial', labels={'VALOR_PAGO': 'Valor Pago', 'MES': 'Mês'})
-    fig_combined.update_traces(mode='lines+markers')
-    st.plotly_chart(fig_combined, use_container_width=True)
-
-    # Texto em letras pequenas abaixo do gráfico
-    st.markdown(f"""
-        <p style='font-size:11px'>
-        Análise Mensal: Criamos um modelo de regressão linear separado para cada mês, utilizando os dados históricos desse mês. <br>
-        Combinação de Dados: Combinamos os dados históricos com as previsões futuras para gerar um gráfico combinado. <br>
-        Acurácia da Previsão: A precisão média das previsões (erro absoluto médio) é de <strong>{locale.currency(avg_mae, grouping=True)}</strong>.<br>
-        Erro Percentual Médio Absoluto (MAPE): <strong>{avg_mape:.2f}%</strong>. <br>
-        </p>
-        """, unsafe_allow_html=True)
-
-    # Função para gerar análise lógica
-    def generate_logical_analysis(df, future_df):
-        current_avg = df['VALOR_PAGO'].mean()
-
-        predicted_max = future_df['VALOR_PAGO_PREVISTO'].max() if not future_df.empty else 0
-        predicted_min = future_df['VALOR_PAGO_PREVISTO'].min() if not future_df.empty else 0
-        predicted_avg = future_df['VALOR_PAGO_PREVISTO'].mean() if not future_df.empty else 0
-
-        # Encontrar o mês atual e o próximo mês
-        current_month = df['MES'].max()
-        next_month = current_month + 1 if current_month < 12 else 1
-
-        # Valores pagos no mês atual e previsto para o próximo mês
-        current_month_value = df[df['MES'] == current_month]['VALOR_PAGO'].mean()
-        next_month_prediction = future_df[future_df['MES'] == next_month]['VALOR_PAGO_PREVISTO'].values[0] if next_month in future_df['MES'].values else 0
-
-        analysis_text = f"""
-        <style>
-        .analysis-text {{
-            font-family: Arial, sans-serif;
-            font-size: 16px;
-            line-height: 1.5;
-        }}
-        </style>
-        <div class="analysis-text">
-        <p>Para os próximos 12 meses, a previsão indica que o valor máximo esperado das despesas será de <strong>{format_currency(predicted_max)}</strong>, 
-        o valor mínimo previsto é de <strong>{format_currency(predicted_min)}</strong>, e a média prevista é de <strong>{format_currency(predicted_avg)}</strong>.</p>
-        <p>Essas previsões sugerem uma tendência de <strong>{'aumento' if predicted_avg > current_avg else 'redução'}</strong> nas despesas. </p>
-        </div>
-        """
-    
-        return analysis_text
-
-    # Gerar o texto para análise lógica
-    logical_analysis_text = generate_logical_analysis(df_meses_ano, future_df_by_month)
-
-    # Exibir o resultado da análise lógica
-    st.subheader("Análise Preditiva dos Dados com Inteligência Artificial")
-    st.markdown(logical_analysis_text, unsafe_allow_html=True)
-
-    # Texto em letras pequenas abaixo do gráfico
-    st.markdown("""
-        <p style='font-size:11px'>
-        Observação sobre a Previsão: <br>
-        Considerando que utilizamos dados históricos de apenas 6 anos para prever o gasto do sétimo ano, é importante ressaltar que a precisão da previsão pode ser limitada. A utilização de uma amostra relativamente pequena de dados pode resultar em uma maior incerteza nas previsões futuras. Portanto, é possível que ocorram grandes disparidades entre os valores previstos e os valores reais devido à falta de informações abrangentes sobre os padrões de gastos ao longo do tempo.
-        </p>
-        """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     run_dashboard()
