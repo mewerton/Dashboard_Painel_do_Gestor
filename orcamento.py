1bdc34152a523b75356fab9b59c34cdbfcf65413
Mewerton Melo <mewerton@gmail.com>
2024-10-24 10:39:47 -0300

    Carregando arquivos necessários par Deploy com o Streamlit

---
+++ orcamento.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import locale
+from sidebar import load_sidebar
+from data_loader import load_data
+
+# Configurar o locale para português do Brasil
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+def run_dashboard():
+    # Carregar dados usando o módulo centralizado
+    df = load_data()
+
+    if df is None:
+        st.error("Nenhum dado foi carregado. Por favor, verifique os arquivos de entrada.")
+        return
+
+    # Carregar o sidebar
+    selected_ugs_despesas, selected_ano, selected_mes = load_sidebar(df, "Orçamento")
+
+   
+
+   
+
+    # Exibir as métricas
+    st.title('Dashboard de Orçamento')
+
+
+if __name__ == "__main__":
+    run_dashboard()
